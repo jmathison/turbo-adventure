5aa340d05a7058ea652e9abc0002d983a71a059e
jmathison <jg.mathison@gmail.com>
2015-07-21 15:50:56 -0700

    Touch controls / Lane control scripts

---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleController : MonoBehaviour {
+
+	[HideInInspector] public GameObject currentLane;
+
+	// Use this for initialization
+	void Start () {
+		setSpawnPos();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate(new Vector3(-.05f,0,0));
+		if(this.transform.position.x < Camera.main.ViewportToScreenPoint(new Vector3(0,0,0)).x ){
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+
+	void setSpawnPos(){
+		Vector3 rightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1,0,0));
+		this.transform.position = rightEdge;
+		snapToLane();
+	}
+
+	void snapToLane(){
+		Vector3 lanePosition = new Vector3(this.transform.position.x, currentLane.transform.position.y, this.transform.position.z);
+		this.GetComponent<SpriteRenderer>().sortingLayerID = currentLane.GetComponent<SpriteRenderer>().sortingLayerID;
+		
+		this.transform.position = lanePosition;
+	}
+}
