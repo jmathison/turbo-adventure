04066a06f47abc7fccb48f84e57484f2ef228de3
jbentley10 <jbentley@idtech.com>
2015-07-21 15:41:43 -0700

    Added menus.

---
+++ Assets/Menus/ButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void LoadLevel()
+	{
+		Application.LoadLevel ("Level1");
+	}
+
+	public void LoadOptions()
+	{
+		Application.LoadLevel ("OptionsMenu");
+	}
+
+	public void LoadMenu ()
+	{
+		Application.LoadLevel ("MainMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
